919a5e1bd5dcbc7c346935c91a89829fe0b0204e
Anton Antonov <anton.synd.antonov@gmail.com>
2016-10-25 18:53:45 +0300

    Rewrite in Elixir

--- app/models/card.rb
+++
@@ -1,8 +0,0 @@
-class Card < ActiveRecord::Base
-  NUMBER_RANGE = (1..14).freeze
-
-  validates :color, uniqueness: { scope: [:number] }
-  validates :number, inclusion: NUMBER_RANGE
-
-  enum color: {spade: 'spade', club: 'club', heart: 'heart', diamond: 'diamond'}.freeze
-end
--- app/models/chat_room.rb
+++
@@ -1,4 +0,0 @@
-class ChatRoom < ActiveRecord::Base
-  has_many :messages, dependent: :delete_all
-  validates :name, presence: true
-end
